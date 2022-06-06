1ec8a4d8d2a849c1b164feb2883806f3c01d3526
it_st it <overult01@gmail.com>
2022-06-06 12:59:18 +0900

    Role 재편

--- src/main/java/dev/road/map/domain/user/Role.java
+++ src/main/java/dev/road/map/domain/user/Role.java
@@ -1,6 +1,18 @@
 package dev.road.map.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
 
-	user, guest
+	// 회원가입 미완료, 자진 탈퇴 
+	GUEST("ROLE_GUEST", "회원가입 미완 회원"),
+	
+	// 회원가입 완료 회원 
+	USER("ROLE_USER","회원"),;
+
+    private final String key;
+    private final String title;
 }
