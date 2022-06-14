043efa1625fc7df6de65696651d47ddbe3d8f1bb
it_st it <overult01@gmail.com>
2022-06-15 01:28:46 +0900

    유저의 필드(백/프론트)에 따른 과목 리스트 조회

--- src/main/java/dev/road/map/domain/SubjectRepository.java
+++ src/main/java/dev/road/map/domain/SubjectRepository.java
@@ -1,7 +1,13 @@
 package dev.road.map.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.road.map.domain.user.Field;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long>{
 	
+	// 필드(백엔드/ 프론트)별 과목 리스트 
+	List<Subject> findByField(Field field);
 }
