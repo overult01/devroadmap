212b5ed6671a6c69bf71f56ea9078c207a58e38f
it_st it <overult01@gmail.com>
2022-06-27 14:19:04 +0900

    friend 컨트롤러, 사비스단 분리

--- src/main/java/dev/road/map/web/FriendController.java
+++ src/main/java/dev/road/map/web/FriendController.java
@@ -23,6 +23,7 @@ import dev.road.map.domain.FriendRepository;
 import dev.road.map.domain.user.Field;
 import dev.road.map.domain.user.User;
 import dev.road.map.domain.user.UserRepository;
+import dev.road.map.service.FriendService;
 
 @RestController
 public class FriendController {
@@ -36,6 +37,9 @@ public class FriendController {
 	@Autowired
 	FriendRepository friendRepository;
 	
+	@Autowired
+	FriendService friendService;
+	
 	@Value("${frontDomain}")
 	String frontDomain;
 	
@@ -47,58 +51,7 @@ public class FriendController {
 		String email = parseUser.parseEmail(request);
 		User user = userRepository.findByEmail(email);
 
-		// 응답 생성 
-    	JsonObject jsonObject = new JsonObject();
-    	JsonArray jsonArray = new JsonArray();    	
-    	
-    	// 친구의 정보들(이메일, 닉네임, 분야)
-//		List<Friend> friendList = friendRepository.selectAllFriends(user);
-//		System.out.println(friendList);
-    	
-    	List<Friend> friends_send = friendRepository.findByUser1AndAcceptAndIsdelete(user, true, false);
-    	List<Friend> friends_recieve = friendRepository.findByUser2AndAcceptAndIsdelete(user, true, false);
-
-    	for(Friend friend_send : friends_send) {
-    		JsonObject jsonObject_inner = new JsonObject();
-
-    		// (본인이 친구 요청, 본인이 user1 = 본인이 user_send_friend)(상대방: user2)
-    		User user_friend = friend_send.getUser2();
-    		String friend_email = user_friend.getEmail();
-    		String friend_nickname = user_friend.getNickname();
-    		Field friend_field = user_friend.getField();
-    		
-    		User friend = userRepository.findByEmail(friend_email);
-    		
-    		jsonObject_inner.addProperty("friend_nickname", friend_nickname);
-    		jsonObject_inner.addProperty("friend_email", friend_email);
-    		jsonObject_inner.addProperty("friend_field", friend_field.toString());
-    		jsonObject_inner.addProperty("friend_progressRate", friend.getProgressRate());  
-
-    		System.out.println(jsonObject_inner);
-    		jsonArray.add(jsonObject_inner);
-    	}
-    	
-    	for(Friend friend_recieve : friends_recieve) {
-    		JsonObject jsonObject_inner = new JsonObject();
-
-    		// (상대방이 친구 요청, 본인이 user2 = 본인이 user_recieve_friend)(상대방: user1)
-    		User user_friend = friend_recieve.getUser1();
-    		String friend_email = user_friend.getEmail();
-    		String friend_nickname = user_friend.getNickname();
-    		Field friend_field = user_friend.getField();
-    		
-    		User friend = userRepository.findByEmail(friend_email);
-    		
-    		jsonObject_inner.addProperty("friend_nickname", friend_nickname);
-    		jsonObject_inner.addProperty("friend_email", friend_email);
-    		jsonObject_inner.addProperty("friend_field", friend_field.toString());  
-    		jsonObject_inner.addProperty("friend_progressRate", friend.getProgressRate());  
-    		
-    		jsonArray.add(jsonObject_inner);
-    	}
-
-    	jsonObject.addProperty("user_email", email);
-    	jsonObject.add("friend_list", jsonArray);
+		JsonObject jsonObject = friendService.friendList(user);
     	
     	// utf-8설정(닉네임에 한글 들어갈 수도 있으니)
 		return ResponseEntity.ok()
